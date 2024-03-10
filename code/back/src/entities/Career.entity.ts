430c7e9d822518993cd5602bc983388cb61586bd
seungsura <seungsura@gmail.com>
2024-03-10 14:30:14 +0900

    dockerfile 테스트

--- code/back/src/entities/Career.entity.ts
+++ code/back/src/entities/Career.entity.ts
@@ -1,43 +1,30 @@
-import {
-  Column,
-  Entity,
-  ManyToOne,
-  PrimaryColumn,
-} from 'typeorm';
+import { Column, Entity, ManyToOne, PrimaryColumn } from 'typeorm';
 
-import { ApiProperty } from '@nestjs/swagger';
 import { User } from './User.entity';
 
-@Entity({name:'career'})
+@Entity({ name: 'career' })
 export class Career {
-@PrimaryColumn({ 
+  @PrimaryColumn({
     name: 'career',
-    type: 'varchar'
+    type: 'varchar',
   })
+  index: number;
 
-// @ApiProperty({
-//   example: '1',
-//   description: 'Primary key ID',
-// })
+  @Column({ type: 'varchar' })
+  period: string;
 
-@Column({type: 'varchar'})
-period: string;
+  @Column({ type: 'varchar' })
+  subtitle: string;
 
-@Column({type: 'varchar'})
-subtitle: string;
+  @Column({ type: 'varchar' })
+  grade: string;
 
-@Column({type:'varchar'})
-grade: string;
+  @Column({ type: 'varchar' })
+  image: string;
 
-@Column({type:'varchar'})
-image: string;
+  @Column({ type: 'varchar' })
+  url: string;
 
-@Column({type:'varchar'})
-url: string;
-
-@ManyToOne(
-  ()=>User,
-  (User)=>User.uid
-  )
-  user!:User;
+  @ManyToOne(() => User, (User) => User.uid)
+  user!: User;
 }
